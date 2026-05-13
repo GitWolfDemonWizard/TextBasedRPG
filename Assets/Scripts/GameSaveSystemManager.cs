338e088219e3f203831a6531c2ecf31d54d3753c
WolfDemon <hank1016@outlook.com>
2026-05-13 15:16:45 -0600

    Added a means for players to receive feed back on saving

--- Assets/Scripts/GameSaveSystemManager.cs
+++ Assets/Scripts/GameSaveSystemManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 public class GameSaveSystemManager : MonoBehaviour
 {
@@ -6,11 +7,21 @@ public class GameSaveSystemManager : MonoBehaviour
 
     private GameData _currentGameData = new();
 
+    private float _timePassed;
+
+    public TextMeshProUGUI SaveText;
+
+    private void Start()
+    {
+        SaveText.text = "";
+    }
     public void SaveGame()
     {
         _currentGameData.PlayData = DialogueManager.ToData();
         SaveManager.Save(_currentGameData);
         Debug.Log("Game Saved");
+        SaveText.text = "Game Saved!";
+        _timePassed = 0;
     }
 
     public void LoadGame()
@@ -20,11 +31,23 @@ public class GameSaveSystemManager : MonoBehaviour
             DialogueManager.FromData(_currentGameData.PlayData);
             Debug.Log("Game Loaded");
             DialogueManager.GoToNode(_currentGameData.PlayData.CurrentNode.NodeID);
+            SaveText.text = "Game Loaded!";
+            _timePassed = 0;
         }
         else
         {
             Debug.LogWarning("No Save found. Creating a new one.");
             SaveGame();
+        }
+    }
+
+    private void Update()
+    {
+        _timePassed += Time.deltaTime;
+
+        if (_timePassed > 5.0)
+        {
+            SaveText.text = "";
         }
     }
 }
